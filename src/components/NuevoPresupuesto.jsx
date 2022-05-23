7879f890705c3eda3e28ac64a4c32ccc22f7d810
Facuscar <facundo.scardillo@dracoder.com>
2022-05-23 08:40:35 -0300

    first commit

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,9 @@
+function NuevoPresupuesto() {
+    return ( 
+        <div className="contenedor-presupuesto contenedor sombra">
+            <h2>Nuevo presupuesto</h2>
+        </div>
+     );
+}
+
+export default NuevoPresupuesto;
