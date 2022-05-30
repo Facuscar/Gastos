bd967c57f92db1c44c94bf261d82b46de888c6cf
Facuscar <facundo.scardillo@dracoder.com>
2022-05-30 18:15:48 -0300

    Add: form in modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -17,6 +17,34 @@ function Modal({setModal, setAnimarModal,animarModal}) {
 
             <form action="" className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
                 <legend>Nuevo gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre gasto</label>
+
+                    <input type="text" placeholder='Añade el nombre del gasto'  id='nombre'/>
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Cantidad</label>
+
+                    <input type="number" placeholder='Añade el monto'  id='cantidad'/>
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Filtrar gastos</label>
+                    <select name="" id="">
+                        <option value="">--Seleccione--</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>     
+                        <option value="casa">Casa</option>     
+                        <option value="gastos">Gastos Varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="subscripciones">Subscripciones</option>  
+                    </select> 
+                </div>
+
+                <input type="submit"  value="Añadir gasto"/>
             </form>
         </div>
      );
