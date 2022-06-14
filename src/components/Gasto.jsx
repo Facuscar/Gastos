1b9b3b336aac801fc5f4acdb72a207d246a19bf9
Facuscar <facundo.scardillo@dracoder.com>
2022-06-14 01:26:00 -0300

    Add: date formatting

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,14 +1,27 @@
+import { formatearFecha } from '../helpers'
+
+import IconoAhorro from '../img/icono_ahorro.svg';
+import IconoCasa from '../img/icono_casa.svg';
+import IconoComida from '../img/icono_comida.svg';
+import IconoGastos from '../img/icono_gastos.svg';
+import IconoOcio from '../img/icono_ocio.svg';
+import IconoSalud from '../img/icono_salud.svg';
+import IconoSuscripciones from '../img/icono_suscripciones.svg';
+
 function Gasto({gasto}) {
     return ( 
     <div className="gasto sombra">
+        {/* Imagen */}
         <div className="contenido-gasto">
             <div className="descripcion-gasto">
                 <p className="categoria">
                     {gasto.categoria}
                 </p>
                 <p className="nombre-gasto">{gasto.nombre}</p>
+                <p className='fecha-gasto'>Agregado el: {''} <span>{formatearFecha(gasto.fecha)}</span></p>
             </div>
         </div>
+        <p className='cantidad-gasto'>${gasto.cantidad}</p>
     </div> 
     );
 }
