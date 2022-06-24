f6719ed74b3965b74ae9c0994be19f1745eb7555
Facuscar <facundo.scardillo@dracoder.com>
2022-06-24 14:49:36 -0300

    Add: Expenses percentage graph

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar'
+import "react-circular-progressbar/dist/styles.css"
 
 function ControlPresupuesto({gastos, presupuesto}) {
 
-    const [disponible, setDisponible] = useState(0)
-    const [gastado, setGastado] = useState(0)
+    const [disponible, setDisponible] = useState(0);
+    const [gastado, setGastado] = useState(0);
+    const [porcentaje, setPorcentaje] = useState(0);
  
     useEffect(() => {
         const totalGastado = gastos.reduce( (total, gasto) => gasto.cantidad + total, 0);
         const totalDisponible = presupuesto - totalGastado;
 
+        //Calcular el porcentaje de lo gastado en cuanto al presupuesto
+        const nuevoPorcentaje =((totalGastado / presupuesto) * 100).toFixed(2);
+        console.log(nuevoPorcentaje);
+        setPorcentaje(nuevoPorcentaje);
         setDisponible(totalDisponible);
         setGastado(totalGastado);
     }, [gastos])
 
     const formatearCantidad = (cantidad) => {
-        return cantidad.toLocaleString('en-US', {
+        return cantidad.toLocaleString('es-ES', {
             style: 'currency',
-            currency: 'USD'
-        })
+            currency: 'ARS',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0,
+        });
     }
     return ( 
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>
-                Grafica aqui
+                <CircularProgressbar value={porcentaje}></CircularProgressbar>
             </div>
             <div className="contenido-presupuesto">
                 <p>
